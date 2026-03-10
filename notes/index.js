77575cdf153bfbfb375174169e3826e6b51ece01
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-03-10 12:53:53 +0300

    Update index.js

--- notes/index.js
+++ notes/index.js
@@ -1,1 +1,26 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const NAME_PROJ = '"NOTE"-"BOOK"';
+
+let notes = [];
+
+let str = `Тебя приветствует приложение ${NAME_PROJ}`;
+
+const addNote = () => {
+  rl.question("Введите заголовок", (title) => {
+    rl.qustion("Напишите текст заметки", (content) => {
+      const newNote = {
+        id: notes.length + 1,
+        title: title,
+        content: content,
+        date: new Date().toLocaleString()
+      };
+    });
+  });
+}; 
 
