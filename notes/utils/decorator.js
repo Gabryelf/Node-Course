d900c616cf07387ea9413406b80cf7fec6cd03a8
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-03-12 11:43:38 +0300

    Update decorator.js

--- notes/utils/decorator.js
+++ notes/utils/decorator.js
@@ -2,5 +2,12 @@ class Decorator{
   static drawLine(){
     console.log("=".repeat(30));
   }
+
+  static presentMenu(welcome){
+    drawLine();
+    console.log('\n');
+    console.log(`${welcome}`);
+    drawLine();
+  }
 }
 module.exports = Decorator;
