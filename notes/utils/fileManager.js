2e79adc4ad8bbe34318e1c3670d24f5da9dee110
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-03-17 12:48:59 +0300

    Update fileManager.js

--- notes/utils/fileManager.js
+++ notes/utils/fileManager.js
@@ -1,1 +1,13 @@
+const fs = require("fs");
+const path = 'notes.json';
 
+const saveFile = (notes) => {
+  const jsonData = JSON.stringify(notes);
+  fs.writeFileSync(path, jsonData);
+};
+
+const loadFile = () => {
+  return fs.readFileSync(path, 'utf-8');
+}
+
+module.exports = { saveFile, loadFile };
