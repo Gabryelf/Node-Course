c210becee60f9efe8119ebdf138922af32c6dff7
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-03-18 08:34:58 +0300

    Update fileManager.js

--- notes/utils/fileManager.js
+++ notes/utils/fileManager.js
@@ -7,8 +7,16 @@ const saveFile = (notes) => {
 };
 
 const loadFile = () => {
-  const jsonData = fs.readFileSync(path, 'utf-8');
-  return JSON.parse(jsonData);
+  try{
+    const jsonData = fs.readFileSync(path, 'utf-8');
+    return JSON.parse(jsonData);
+  }
+  catch(error){
+    console.log(`${error.massage}`);
+    console.log('Возникла ошибка', error.massage);
+    return [];
+  }
+  
 }
 
 module.exports = { saveFile, loadFile };
