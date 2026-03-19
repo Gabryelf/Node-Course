86d8bee2cd58941a463c73e1b960f75c026a4a58
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-03-19 09:49:35 +0300

    Create server.js

---
+++ notes/server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const fs = require('fs').promises;
+const path = require('path');
+
+const helper = require("./utils/helper");
+const fileManager = require("./utils/fileManager");
+
+let notes = fileManager.loadFile();
+
+const server = http.createServer(async (req, res) => {
+  const { url, method } = req;
+});
+
+server.listen(3000, () => {
+  console.log("Сервер запущен на порту http://localhost:3000");
+});
+
