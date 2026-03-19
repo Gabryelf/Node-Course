515f7515dfbdd18771f6095b4ef40f352237b40e
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-03-19 12:41:07 +0300

    Update app.js

--- notes/app.js
+++ notes/app.js
@@ -1,37 +1,31 @@
-const stats = document.getElementById('stats');
-const content = document.getElementById('content');
+const stats = document.getElementById("stats");
+const content = document.getElementById("content");
 
-let notes = []
+let notes = [];
 
-async function loadNotes(){
-  try{
-    const res = await fetch('/api/notes/');
+async function loadNotes() {
+  try {
+    const res = await fetch("api/notes");
     notes = await res.json();
-    stats.innerText = `Заметок ${notes.length}`
-  }
-  catch(error){
+    stats.innerText = `Заметок ${notes.length}`;
+  } catch (error) {
     console.log("Ощибка", error);
-    stats.innerText = `Информации о заметках нет`
+    stats.innerText = `Информации о заметках нет`;
   }
 }
 
-async function addNote(){
+async function addNote() {
   const title = prompt("Введите название ");
   const content = prompt("Введите содержание ");
-
-  try{
-    await fetch('api/notes', {
+  try {
+    await fetch("api/notes", {
       method: "POST",
-      headers: {'Content-Type' : 'application/json' },
-      body: JSON.stringify({title, content})
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title, content }),
     });
-  }
-  catch(error)
-  {
-    console.log("ERROR", error.message)
+  } catch (error) {
+    console.log("ERROR", error.message);
   }
 }
 
 loadNotes();
-
-
