17c1a85701a662c987ad5c748aaebd618c564774
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-03-19 10:12:06 +0300

    Update app.js

--- notes/app.js
+++ notes/app.js
@@ -1,1 +1,16 @@
+const stats = document.getElemettById('stats');
+const content = document.getElemettById('content');
 
+let notes = []
+
+async function loadNotes(){
+  try{
+    const res = await fetch('/api/notes/');
+    notes = await res.json();
+    stats.innerText = `Заметок ${notes.length}`
+  }
+  catch(error){
+    console.log("Ощибка", error);
+    stats.innerText = `Информации о заметках нет`
+  }
+}
